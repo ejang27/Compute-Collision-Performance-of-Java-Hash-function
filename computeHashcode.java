ff01a1c8af8a71dfa64eb4fd707cc536114b7e95
Eunsoo Jang <43043990+ejang27@users.noreply.github.com>
2020-02-25 16:59:09 -0500

    commit1

---
+++ computeHashcode.java
@@ -0,0 +1,27 @@
+
+
+//Tite: computeHashcode.java
+//Written by: Eunsoo Jang
+//Date: 02/25/2020
+
+import java.io.*;
+import java.util.*;
+
+public class computeHashcode
+{
+  public static void main(String[] args){
+    String s = "Bryn Mawr";
+    System.out.println(s.hashCode());
+
+    s = "K-Cass";
+    System.out.println(s.hashCode());
+
+    s = "Haverford";
+    System.out.println(s.hashCode());
+
+    s = "Swarthmore";
+    System.out.println(s.hashCode());
+
+  }
+
+}
